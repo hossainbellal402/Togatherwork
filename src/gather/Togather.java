e314285115b00a9763bf8607e81cdf75ae776845
hossainbellal402 <66582075+hossainbellal402@users.noreply.github.com>
2020-06-28 01:42:44 -0400

    adding

--- src/gather/Togather.java
+++ src/gather/Togather.java
@@ -4,42 +4,71 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class Togather extends Totry {
 	
-public String url ="https://www.Facebook.com/";
-public String fn ="//*[@id=\"u_0_m\"]";
+public String url ="https://www.facebook.com/";
+public String fn ="//input[@id='u_0_m']";
 public String fval="Billa";
-public String ln ="";
-public String ea ="";
-public String pw ="";
-public String month = "";
-public String day = "";
-public String year = "";
-public String gender = "";
-public String singup = "";
+public String ln ="//input[@id='u_0_o']";
+public String lst = "Hossain";
+
+//  public String ea ="";
+//  public String pw ="";
+//  public String month = "";
+//  public String day = "";
+//  public String year = "";
+//  public String gender = "";
+//  public String singup = "";
 	
 
 @BeforeTest
 	
 	public void OpenBrowers() {
 		
-System.setProperty( "webdriver.chrome.driver", "C:\\Users\\bella\\eclipse-workspace\\Togatherwork\\Drivers\\chromedriver.exe");	
+System.setProperty( "webdriver.chrome.driver", "C:\\Users\\bella\\eclipse-workspace\\Togatherwork\\Drivers\\chromedriver.exe");
+ //System.setProperty("webdriver.opera.driver", "C:\\Users\\bella\\eclipse-workspace\\MTaBH007\\drivers\\operadriver.exe");
+  //System.setProperty("webdriver.gecko.driver", "C:\\Users\\bella\\eclipse-workspace\\MTaBH007\\drivers\\geckodriver.exe");
+
     driver = new ChromeDriver();
-	driver.get( "url");	
+   // driver = new OperaDriver();
+    //driver = new FirefoxDriver();
+    		
+	driver.get( url);	
 	driver.manage().deleteAllCookies();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait( 45, TimeUnit.SECONDS);
-		 driver.close(); 
+		 
 		 System.out.println("Chrome driver open");
-		
+		}
+@Test
 
-	}
 public void singupPage() {
 	System.out.println("singup page testing......");
 	driver.findElement(By.xpath(fn)).sendKeys(fval);
+	driver.findElement(By.xpath(ln)).sendKeys( lst);
 }
+@AfterMethod
+public void closeBrowers() {
+	System.out.println("This is the close Browers testing==========");
+	driver.close();
+	System.out.println("This is the Browers close+++++++++++");
+	}
+@AfterSuite
+public void TearDown(){
+	driver.quit();
+	System.out.println("Termanated ...........");
 
+	
+	
+	
+	
+}
 
 }
